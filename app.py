096a7ee177e5d12eac215446464a3743cf2ffafd
paul-ontita <ontitapaul.po@gmail.com>
2021-04-16 02:42:11 +0300

    dash app

--- app.py
+++ app.py
@@ -8,6 +8,19 @@ from dash.dependencies import  Input, Output
 
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__,external_stylesheets = external_stylesheets)
@@ -21,7 +34,9 @@ app.layout = html.Div([
         value = df['year'].min(),
         marks = {str(year):str(year) for year in df['year'].unique()},
         step =None
-    )
+    ),
+    html.H4(children='World GDP data'),
+    generate_table(df)
 ])
 
 
@@ -35,9 +50,11 @@ def update_figure(year_selected):
 
     fig = px.scatter(filtered_df,x="gdpPercap", y="lifeExp",
                      size="pop", color="continent", hover_name="country",
-                     log_x=True, size_max=55)
+                     log_x=True, size_max=55,title = 'My Graph')
     fig.update_layout(transition_duration = 500) 
-    return fig               
+    return fig    
+    
+               
 
 
 if __name__ == '__main__':
